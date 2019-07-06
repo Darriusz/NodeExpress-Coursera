9fc1a4d497eadc9ce0bd0d2e814a18ca29a8ab9f
Darriusz <51857639+Darriusz@users.noreply.github.com>
2019-07-06 23:08:14 +0200

    3 routers

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require ('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,39 +14,16 @@ const app = express();
 app.use(morgan('dev')); //daje na konsoli inf o komunikacji z serwerem
 app.use(bodyParser.json());
 
-app.use('/dishes', dishRouter); //requests coming to /dishes will be handled by dishRouter
-
-
-//*** REST Representational State Transfer *************************
-
-
-app.get('/dishes/:dishId', (req, res, next) => {
-	res.end('When integrated with MongoDB it will give details of the dish:' + req.params.dishId + ' to you');
-});
-
-app.post('/dishes/:dishId/', (req, res, next) => {
-	res.statusCode = 403;
-	res.end('POST operation not done on existing dishes');
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-	res.write('Updating the dish: ' + req.params.dishId + "\n");
-	res.end('When integrated with MongoDB it will update the dish: ' + req.body.name + 
-		' with details ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-	res.end('When integrated with MongoDB it will delete dish: ' + req.params.dishId);
-});
-
-//*************end of REST *****************
+app.use('/dishes', dishRouter); 
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/pub'));
 
 app.use((req, res, next) => {
 
 	var fileUrl = req.url;
-	if (fileUrl == '/wazneDane.html'){
+	if (fileUrl == '/importantData.html'){
 		res.statusCode = 403;
 		res.setHeader('Content-Type', 'text/html');
 		res.end('<html><body><h1>Error 403: for ' + fileUrl + ' access forbidden</h1></body></html>');
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -11,7 +11,7 @@ dishRouter.route('/')
 	next(); //to continue further operations below for /dishes
 })
 .get((req, res, next) => {
-	res.end('Later on it will retrieve json data drom a MongoDB here');
+	res.end('Later on it will retrieve json data from a MongoDB here');
 })
 .post((req, res, next) => {
 	res.end('When integrated with MongoDB it will add the dish name: ' + req.body.name + 
@@ -23,6 +23,24 @@ dishRouter.route('/')
 })
 .delete((req, res, next) => {
 	res.end('Deleting all the dishes when integrated with MongoDB!!!');
+});
+
+
+dishRouter.route('/:dishId')
+.get((req, res, next) => {
+	res.end('When integrated with MongoDB it will give details of the dish:' + req.params.dishId + ' to you');
+})
+.post((req, res, next) => {
+	res.statusCode = 403;
+	res.end('POST operation not done on existing dishes');
+})
+.put((req, res, next) => {
+	res.write('Updating the dish: ' + req.params.dishId + "\n");
+	res.end('When integrated with MongoDB it will update the dish called: ' + req.body.name + 
+		' with the following details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+	res.end('When integrated with MongoDB it will delete dish: ' + req.params.dishId);
 });
 
 
