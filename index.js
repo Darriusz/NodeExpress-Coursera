95bca9ef8b1d67b0e14ea9c489acb3b5aa28d6dc
Darriusz <51857639+Darriusz@users.noreply.github.com>
2019-07-06 19:01:38 +0200

    error msgs

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan('dev')); //daje na konsoli inf o komunikacji z serwerem
 
-app.use(express.static(__dirname+ '/pub'))
+app.use(express.static(__dirname+ '/pub'));
 
 app.use((req, res, next) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html')
-	res.end('<html><body><h1>hello, this is an Express server!</h1></body></html>');
+
+	var fileUrl = req.url;
+	if (fileUrl == '/wazneDane.html'){
+		res.statusCode = 403;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<html><body><h1>Error 403: for ' + fileUrl + ' access forbidden</h1></body></html>');
+		return;			
+	}
+
+	var filePath = path.resolve('./pub'+fileUrl);
+	const fileExt = path.extname(filePath);
+	if (fileExt=='.html') {
+		fs.exists(filePath, (exists) => {
+			if (!exists) {
+				res.statusCode = 404;
+				res.setHeader('Content-Type', 'text/html');
+				res.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>');
+				return;
+			}
+
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html')
+		res.end('<html><body><h1>hello, this is an Express server!</h1></body></html>');
+		}); 
+	}
+	else {
+	res.statusCode = 404;
+	res.setHeader('Content-Type', 'text/html');
+	res.end('<html><body><h1>Error 404: ' + fileExt + ' files not supported</h1></body></html>');
+	return;			
+	}
+
 });
 
 const server = http.createServer(app);
